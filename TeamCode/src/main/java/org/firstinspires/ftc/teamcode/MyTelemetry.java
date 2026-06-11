ed41a6f8a824838e5dc95f0eae33d4a62fcc3931
SahalG <sahalgoyal@icloud.com>
2026-06-11 10:42:13 -0400

    Basic hardware mapping

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTelemetry.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class MyTelemetry {
+    public static MultipleTelemetry telemetry;
+    public static void init(Telemetry tele) {
+        telemetry = new MultipleTelemetry(tele, FtcDashboard.getInstance().getTelemetry());
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class Robot {
+    private static ElapsedTime elapsedTime;
+    DcMotor leftFront, rightFront, backLeft, backRight;
+
+    public Robot (HardwareMap hardwareMap) {
+        elapsedTime = new ElapsedTime();
+        elapsedTime.reset();
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+    }
 }
